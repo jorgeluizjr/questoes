66a060574fecc0b0c01dc1e81ef50098420b4b97
Jorge Luiz Jr <juninhoecb@gmail.com>
2020-01-27 19:10:41 -0300

    [UPDATE] Refatora arquivos para corrigir erro de lint;

--- webapp/src/modules/shared/service/base/index.js
+++ webapp/src/modules/shared/service/base/index.js
@@ -1,46 +1,7 @@
 import axios from 'axios';
-import { eventHub } from '@/event';
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_HOST,
 });
 
-export const buildData = function (obj, form, namespace) {
-  const fd = form || new FormData();
-  let formKey;
-
-  for (const property in obj) {
-    if (obj.hasOwnProperty(property)) {
-      if (namespace) {
-        formKey = `${namespace}[${property}]`;
-      } else {
-        formKey = property;
-      }
-
-      // if the property is an object, but not a File,
-      // use recursivity.
-      if (typeof obj[property] === 'object' && !(obj[property] instanceof File)) {
-        buildData(obj[property], fd, property);
-      } else {
-        // if it's a string or a File object
-        fd.append(formKey, obj[property]);
-      }
-    }
-  }
-
-  return fd;
-};
-
 export const getRequest = (path, config = {}) => instance.get(path, config);
-
-export const postRequest = (path, payload) => instance.post(path, payload);
-
-export const putRequest = (path, id, payload) => instance.put(`${path}/${id}`, payload);
-
-export const patchRequest = (path, id, payload) => instance.patch(`${path}/${id}`, payload);
-
-export const deleteRequest = (path, id) => instance.delete(`${path}/${id}`);
-
-export const replaceAll = function (searchString, replaceString, str) {
-  return str.split(searchString).join(replaceString);
-};
--- webapp/src/modules/shared/service/shared.js
+++ webapp/src/modules/shared/service/shared.js
@@ -1,6 +1,1 @@
 import * as service from './base/index';
-/* eslint-disable import/prefer-default-export */
-
-// `${path}/${id}`
-export const downloadArquivo = coArquivo => service.downloadArquivo(coArquivo);
-export const obterBinarioArquivo = coArquivo => service.obterBinarioArquivo(coArquivo);
